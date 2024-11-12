20e20c76a871a52835d8693c725171971bf974c0
aristides ricardo fioretti <arfioretti@gmail.com>
2024-11-12 02:16:33 -0300

    91-load categories table

---
+++ YumBlazor/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Please enter Name...")]
+        public string Name { get; set; }
+    }
+}
